fafc75e983bc653c28dd50fc0a968980daee017f
jonathan.l <jonathan.l@hdcorp.co.uk>
2024-01-12 09:54:14 +0000

    unresolved issue with no arm v7 docker image available

--- install_docker_jenkins.sh
+++ install_docker_jenkins.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+#
+# Script to install a docker image of Jenkins on either an Ubuntu or Raspberry pi.
+# Error with installation on to Raspberry Pi states, no docker image available for "arm v7" 01/01/24.
+#
+
 
 set -eu -o pipefail
 
@@ -12,7 +17,7 @@ echo ...
 
 echo "installing the must-have pre-requisites"
 sudo apt-get install python3-pip -y
-while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+while read -r p ; do sudo apt-get install -y "$p" ; done < <(cat << "EOF"
   git
   docker
   openjdk-8-jre
@@ -22,13 +27,15 @@ EOF
 echo ...
 echo ...
 echo "need to check for ubuntu or arm"
+# wouterds/rpi-jenkins, jenkins-arm64
+# https://raspberrytips.com/install-jenkins-raspberry-pi/
 host_linux=$(uname -v | awk '{ print $1 }' | sed -n 's/.*U//p' ) # expect 21-Ubuntu
 host_arm=$(uname -a | awk '{ print $11 }' | grep -oP "arm") # expecting armv...
-if [ $host_linux == "buntu" ]; then
+if [ "$host_linux" == "buntu" ]; then
   echo "installing docker image jenkins/jenkins for ubuntu"
   docker pull jenkins/jenkins
 fi
-if [ $host_arm == 'arm' ]; then
+if [ "$host_arm" == 'arm' ]; then
   echo "installing docker image mlucken/jenkins-arm for arm"
   docker pull mlucken/jenkins-arm
 fi
@@ -39,14 +46,14 @@ echo ...
 echo "running image on port 3000"
 sudo docker run -p 3000:8080 -d --name oscar jenkins/jenkins
 variable_2=$(sudo docker ps)
-echo $variable_2
-echo cat $variable_2 | egrep "oscar"
-variable_1=$(cat $variable_2 | awk '{ print $1 }' | awk '/^[0-9]|[a-z]/ { print }'})
+echo "$variable_2"
+echo cat "$variable_2" | grep -e "oscar"
+variable_1=$(cat "$variable_2" | awk '{ print $1 }' | awk '/^[0-9]|[a-z]/ { print }')
 echo "this is the short version docker image ID ... $variable_1"
 echo ...
 echo ...
 echo ...
 sleep 10 # need a pause for the image to run before grabbing the password
-variable_password=$(sudo docker exec $variable_1 cat /var/jenkins_home/secrets/initialAdminPassword)
+variable_password=$(sudo docker exec "$variable_1" cat /var/jenkins_home/secrets/initialAdminPassword)
 echo "admin password is ... $variable_password"
 echo .................
